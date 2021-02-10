fe113a3a706d0f2b60395926a0ae3e67e945e5e9
m01190442c <m01190442c@edu.teu.ac.jp>
2021-02-10 22:05:10 +0900

    江藤進捗１

---
+++ Assets/spin2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin2 : MonoBehaviour
+{
+    public huge HUGE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            //torque = Vector3.left;
+            //float turn = Input.GetAxis("Horizontal");
+            //rb.AddTorque(transform.up * torque * turn);
+            //rb.AddTorque(torque, ForceMode.Acceleration);
+            //rb.AddForceAtPosition(torque, ForceMode.Acceleration, transform.position);
+            transform.Rotate(new Vector3(0, -10, 0));
+            HUGE.hugeScale();
+        }
+    }
+}
