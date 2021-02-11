f4547a0b7ebd7412d495a1c68c5447da9b5e8122
Satoshi Aikawa <m0119001bb@edu.teu.ac.jp>
2021-02-12 01:28:18 +0900

    カメラの貫通(床だけ)

--- Assets/Script/FollowingCamera.cs
+++ Assets/Script/FollowingCamera.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// The camera added this script will follow the specified object.
@@ -9,26 +11,45 @@ using UnityEngine.UI;
 public class FollowingCamera : MonoBehaviour
 {
     public GameObject target; // an object to follow
+    //public GameObject surface;
+
     public Vector3 offset; // offset form the target object
-
     public GameObject camera_view = null;
-
-    bool flag = false; 
+    bool flag = false;
 
     [SerializeField] private float distance = 7.0f; // distance from following object
     [SerializeField] private float polarAngle = 20.0f; // angle with y-axis
     [SerializeField] private float azimuthalAngle = 270.0f; // angle with x-axis
 
-    [SerializeField] private float minDistance = 3.0f;
-    [SerializeField] private float maxDistance = 20.0f;
+    [SerializeField] private float minDistance = 5.0f;
+    [SerializeField] private float maxDistance = 10.0f;
     [SerializeField] private float minPolarAngle = 30.0f;
     [SerializeField] private float maxPolarAngle = 140.0f;
     [SerializeField] private float mouseXSensitivity = 5.0f;
     [SerializeField] private float mouseYSensitivity = 5.0f;
     [SerializeField] private float scrollSensitivity = 5.0f;
 
+    //壁貫通
+
+    //
+    private void OnTriggerExit(Collider other)
+    {
+        distance = 10.0f;
+    }
     void Update()
     {
+        Vector3 Target = target.transform.position;
+        Ray ray = new Ray(Target, transform.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            float dis = Vector3.Distance(Target, hit.point);
+            Debug.Log(distance);
+            transform.position = hit.point;
+            distance = 2.0f;
+        }
+        Debug.DrawLine(Target, transform.position, Color.red, 0f, false);
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (flag == false)
@@ -40,7 +61,6 @@ public class FollowingCamera : MonoBehaviour
                 flag = false;
             }
         }
-        
     }
 
     void LateUpdate()
@@ -56,24 +76,25 @@ public class FollowingCamera : MonoBehaviour
         updatePosition(lookAtPos);
         transform.LookAt(lookAtPos);
     }
-
     void updateAngle(float x, float y)
     {
         //Mouseの左長押しでCameraのアングル固定　//KeyboardでCamera固定
-        if (Input.GetMouseButton(0)||flag == true)
+        if (Input.GetMouseButton(0) || flag == true)
         {
-            Debug.Log("trueです--------------------------------");
+            Cursor.visible = true;
+            //Debug.Log("trueです--------------------------------");
 
             Text view_text = camera_view.GetComponent<Text>();
-            view_text.text = "カメラ固定 : ON"  ;
+            view_text.text = "カメラ固定 : ON";
         }
-        else　
+        else
         {
+            Cursor.visible = false;
+            //Debug.Log("falseです");
             x = azimuthalAngle - x * mouseXSensitivity;
             azimuthalAngle = Mathf.Repeat(x, 360);
             y = polarAngle + y * mouseYSensitivity;
             polarAngle = Mathf.Clamp(y, minPolarAngle, maxPolarAngle);
-            Debug.Log("falseです");
 
             Text view_text = camera_view.GetComponent<Text>();
             view_text.text = "カメラ固定 : OFF";
