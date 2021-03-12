e64b52599ec75bf518cd4a4e0386515d14509960
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-03-12 23:16:53 +0900

    ３Dモデルテスト

---
+++ Assets/Script/Serron/MoveTest01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest01 : MonoBehaviour
+{
+	public float speed = 3.0f;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey("up"))
+		{
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("down"))
+		{
+			transform.position -= transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("right"))
+		{
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("left"))
+		{
+			transform.position -= transform.right * speed * Time.deltaTime;
+		}
+	}
+}
