93814ed0ce6314badef8903840e577dd3d16a8fe
YutaKurosawa <m0119114c4@edu.teu.ac.jp>
2021-03-16 17:37:31 +0900

    画面上でマウスを動かすことで餅を回転させ、餅を大きくする機能の追加

---
+++ Assets/Script/Kurosawa/MotiRotate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotiRotate : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    Vector2 mPos;
+    Vector3 screenSizeHalf;
+    public MotiHuge HUGE;
+    float rad;
+    float previousRad;
+    float tan = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 画面の縦横の半分 
+        screenSizeHalf.x = Screen.width / 2f;
+        screenSizeHalf.y = Screen.height / 2f;
+        screenSizeHalf.z = 0f;
+
+        // マウスの初期位置
+        mPos = Input.mousePosition - screenSizeHalf;
+        previousRad = Mathf.Atan2(mPos.x, mPos.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 真ん中が(0,0,0)になるようにマウスの位置を取得
+        mPos = Input.mousePosition - screenSizeHalf;
+
+        float rad = Mathf.Atan2(mPos.x, mPos.y); // 上向きとマウス位置のなす角
+        float dRad = rad - previousRad; // 前のフレームの角度との差
+        if (Input.GetMouseButton(0))
+        {
+            tan += Mathf.Tan(dRad); //タンジェント // * mPos.magnitude;
+            Player.transform.Rotate(new Vector3(0, tan / 50, 0));
+            HUGE.hugeScale();
+        }
+        previousRad = rad; // 今のフレームの角度を保存
+        if (Input.GetMouseButtonUp(0))
+        {
+            tan = 0;
+            HUGE.ResetE();
+        }
+    }
+}
+
