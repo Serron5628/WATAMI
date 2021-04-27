bcb4810343266918722d6218a28156bf56797385
daigennki <daigennki@users.noreply.github.com>
2021-04-27 13:56:15 +0900

    競合解決

--- Assets/Script/Kurosawa/MotiRotate.cs
+++ Assets/Script/Kurosawa/MotiRotate.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MotiRotate : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] GameObject Player;
     Vector2 mPos;
     Vector3 screenSizeHalf;
@@ -13,15 +12,6 @@ public class MotiRotate : MonoBehaviour
     float previousRad;
     float tan = 0f;
     float RotationCount = 0;
-=======
-    [SerializeField] GameObject Cube;
-
-    Vector2 mPos;
-    Vector3 screenSizeHalf;
-    float rad;
-    float previousRad;
-    float tan = 0f;
->>>>>>> origin/master
 
     // Start is called before the first frame update
     void Start()
@@ -47,7 +37,6 @@ public class MotiRotate : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             tan += Mathf.Tan(dRad); //タンジェント // * mPos.magnitude;
-<<<<<<< HEAD
             Player.transform.Rotate(new Vector3(0, tan / 50, 0));//プレイヤーの回転
 
             if(dRad>1||dRad<-1) //フレームの角度の差が1以上あれば餅伸ばし実行
@@ -59,14 +48,10 @@ public class MotiRotate : MonoBehaviour
             {
                 HUGE.hugeScale();//餅伸ばし開始
             }
-=======
-            Cube.transform.Rotate(new Vector3(0, tan / 50, 0));
->>>>>>> origin/master
         }
         previousRad = rad; // 今のフレームの角度を保存
         if (Input.GetMouseButtonUp(0))
         {
-<<<<<<< HEAD
             RotationCount = 0;//回転数リセット
             tan = 0;
             HUGE.ResetE();//餅の大きさリセット
@@ -74,9 +59,3 @@ public class MotiRotate : MonoBehaviour
     }
 }
 
-=======
-            tan = 0;
-        }
-    }
-}
->>>>>>> origin/master
