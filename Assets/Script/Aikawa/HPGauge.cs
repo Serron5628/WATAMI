2f9a51adffbcdfc1ad85ec99c1092af578d9bfe3
Satoshi Aikawa <m0119001bb@edu.teu.ac.jp>
2021-05-05 06:39:47 +0900

    一時保存、数字変更まで

---
+++ Assets/Script/Aikawa/HPGauge.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class HPGauge : MonoBehaviour
+{
+
+    public GameObject objHPGauge, objTextHPValue;
+    private Image imgHPGauge;
+    private Text textHP;
+    private int maxHp, hp;
+
+    // Use this for initialization
+    void Start()
+    {
+        imgHPGauge = objHPGauge.GetComponent<Image>();
+        textHP = objTextHPValue.GetComponent<Text>();
+    }
+
+    public int MaxHP
+    {
+        set { maxHp = value; }
+    }
+
+    public int HP
+    {
+        set { hp = value; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        imgHPGauge.fillAmount = (float)hp / maxHp;
+        textHP.text = hp.ToString();
+    }
+}
