3b5d1aa1c712f2062ce8dacb9742f58e15318e39
Yuta Urano <m0119041e8@edu.teu.ac.jp>
2021-05-08 13:47:05 +0900

    エラー修正

--- Assets/Script/Kurosawa/MotiRotate.cs
+++ Assets/Script/Kurosawa/MotiRotate.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class MotiRotate : MonoBehaviour
 {
-<<<<<<< HEAD
-//<<<<<<< HEAD
-=======
->>>>>>> origin/master
     [SerializeField] GameObject Player;
     Vector2 mPos;
     Vector3 screenSizeHalf;
@@ -16,18 +12,6 @@ public class MotiRotate : MonoBehaviour
     float previousRad;
     float tan = 0f;
     float RotationCount = 0;
-<<<<<<< HEAD
-    //=======
-    //[SerializeField] GameObject Cube;
-
-    //Vector2 mPos;
-    //Vector3 screenSizeHalf;
-    //float rad;
-    //float previousRad;
-    //float tan = 0f;
-//>>>>>>> origin/master
-=======
->>>>>>> origin/master
 
     // Start is called before the first frame update
     void Start()
@@ -53,10 +37,6 @@ public class MotiRotate : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             tan += Mathf.Tan(dRad); //タンジェント // * mPos.magnitude;
-<<<<<<< HEAD
-                                    //<<<<<<< HEAD
-=======
->>>>>>> origin/master
             Player.transform.Rotate(new Vector3(0, tan / 50, 0));//プレイヤーの回転
 
             if (dRad > 1 || dRad < -1) //フレームの角度の差が1以上あれば餅伸ばし実行
@@ -68,20 +48,10 @@ public class MotiRotate : MonoBehaviour
             {
                 HUGE.hugeScale();//餅伸ばし開始
             }
-<<<<<<< HEAD
-            //=======
-            //Cube.transform.Rotate(new Vector3(0, tan / 50, 0));
-//>>>>>>> origin/master
-=======
->>>>>>> origin/master
         }
         previousRad = rad; // 今のフレームの角度を保存
         if (Input.GetMouseButtonUp(0))
         {
-<<<<<<< HEAD
-            ////<<<<<<< HEAD
-=======
->>>>>>> origin/master
             RotationCount = 0;//回転数リセット
             tan = 0;
             HUGE.ResetE();//餅の大きさリセット
@@ -89,12 +59,3 @@ public class MotiRotate : MonoBehaviour
     }
 }
 
-<<<<<<< HEAD
-//=======
-//tan = 0;
-//        }
-//    }
-//}
-//>>>>>>> origin/master
-=======
->>>>>>> origin/master
