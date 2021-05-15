e2cac99d64f92418a3d3fc0541cf16c6ef8d4957
m01190442c <m01190442c@edu.teu.ac.jp>
2021-05-15 14:20:18 +0900

    くっつけたもののSizeに関するスクリプト

---
+++ Assets/Script/EtoScript/ReleaseE.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleaseE : MonoBehaviour
+{
+    public StickE4 stickE4;
+    Rigidbody rb;
+    public bool rotational2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OntriggerEnter(Collider other)
+    {
+        rotational2 = stickE4.rotationOn;
+        if (other.gameObject.name=="Hako1")
+        {
+            other.transform.parent = null;
+            rb.isKinematic = false;
+            rotational2 = true;
+        }
+    }
+}
--- Assets/Script/EtoScript/StickE.cs
+++ Assets/Script/EtoScript/StickE.cs
@@ -12,13 +12,15 @@ public class StickE : MonoBehaviour
     public bool flyOn;
     private bool parentOn;
     public bool ReParent;
+    public Vector3 defaultScale = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
         flyOn = false;
         parentOn = false;
         ReParent = true;
-        
+        defaultScale = transform.lossyScale;
+
     }
         
         // Update is called once per frame
@@ -46,7 +48,13 @@ public class StickE : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
         if(fixedJoint==null&&ReParent==true&&other.gameObject.name=="MOTI")
         {
-            this.gameObject.transform.parent = moti.gameObject.transform;
+            this.gameObject.transform.parent=moti.gameObject.transform;
+            //Vector3 scale = transform.localScale;
+            //scale.x /= moti.localScale.x;
+            //scale.y /= moti.localScale.y;
+            //scale.z /= moti.localScale.z;
+            //transform.localScale = scale;
+            //this.gameObject.transform.SetParent(transform, false);
             rb.isKinematic = true;
             //transform.localPosition = new Vector3(0, 0, 0);
             parentOn = true;
