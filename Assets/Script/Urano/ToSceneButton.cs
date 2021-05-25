3d9e09d7c217970703cc80214482fdbe12b615f5
Yuta Urano <m0119041e8@edu.teu.ac.jp>
2021-05-26 00:05:43 +0900

    スタート画面追加

---
+++ Assets/Script/Urano/ToSceneButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToSceneButton : MonoBehaviour
+{
+    [SerializeField]
+    string nextStageName;
+    [SerializeField]
+    float waitTime;
+    public void NextScene()
+    {
+        Invoke("ChangeScene", waitTime);
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(nextStageName);
+    }
+}
