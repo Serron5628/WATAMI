36fbca5166c8f13bf5a7cb76fdd84d49e3d1f92f
Satoshi Aikawa <m0119001bb@edu.teu.ac.jp>
2021-06-12 01:21:56 +0900

    マスターからもってきた

---
+++ Assets/Script/Aikawa/CharaJumpCtrl_2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaJumpCtrl_2 : MonoBehaviour
+{
+    private Animator usagi;
+    private string usagiStr = "isHissatu";
+    public static bool HissatuAnim = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.usagi = GetComponent<Animator>();
+        this.usagi.SetBool(usagiStr, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            HissatuAnim = true;
+        }
+        else
+        {
+            if(Input.GetMouseButton(0))
+            {
+                HissatuAnim = false;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        if (HissatuAnim)
+        {
+            this.usagi.SetBool(usagiStr, true);
+        }
+        else
+        {
+            if (HissatuAnim == false)
+            {
+                this.usagi.SetBool(usagiStr, false);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Aikawa/jumpAfterDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpAfterDamage : CharaJumpCtrl_2{
+    public static bool attack = false;
+    public void FixedUpdate(){
+        
+        if(HissatuAnim == false){
+            Debug.Log("振り下ろし確認");
+            if(attack==true)LastAttack();
+        }else{
+
+        }
+    }
+    public void LastAttack(){
+
+    }
+    public void WaitAttack(){
+
+    }
+}
