e161b9e0e9ad74979286fcdac82d214ce04dcbe3
Yuta Urano <m0119041e8@edu.teu.ac.jp>
2021-06-12 05:00:06 +0900

    スタート画面 光の調整とボタンの追加

---
+++ Assets/Script/Urano/EndGameButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameButton : MonoBehaviour
+{
+    [SerializeField]
+    float endTime ;
+    public void EndGame()
+    {
+        Invoke("QuitGame", endTime);
+    }
+
+    void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+        UnityEngine.Application.Quit();
+        #endif
+    }
+}
