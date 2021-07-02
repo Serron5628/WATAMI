5da9fe616df83c970b930fa7806a9ba0e40433c6
Satoshi Aikawa <m0119001bb@edu.teu.ac.jp>
2021-07-03 00:08:43 +0900

    たたき落とし後にEnemy削除

---
+++ Assets/Script/Aikawa/EnemyDestroy_02.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemyDestroy_02 : MonoBehaviour{
+    [SerializeField] private GameObject ParentObject;
+    private GameObject[] ChildObject;
+    private void GetAllChildObject(){
+        ChildObject = new GameObject[ParentObject.transform.childCount];
+        for (int i = 0; i < ChildObject.Length; i++){
+            if(ParentObject.transform.GetChild(i).tag == "enemy")
+                ChildObject[i] = ParentObject.transform.GetChild(i).gameObject;
+        }
+    }
+    public void DestroyEnemy(){
+        for (int i = 0; i < ChildObject.Length; i++){
+            if(ParentObject.transform.GetChild(i).tag == "enemy")
+                Destroy(ChildObject[i]);
+        }
+    }
+}
