5941bd541546c680da2148a58acb9af3caa67a43
Yuta Urano <m0119041e8@edu.teu.ac.jp>
2021-07-07 00:35:16 +0900

    クオータービュー 提案の確認

---
+++ Assets/Script/Urano/stay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stay : MonoBehaviour
+{
+    [SerializeField]
+    GameObject stObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = stObj.transform.position;
+    }
+}
