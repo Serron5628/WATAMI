57139de24c645fc6b2f5c5e2f8cff7300f053ac0
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-09-01 01:47:09 +0900

    errorはいてる

---
+++ Assets/Script/Serron/RunSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunSound : MonoBehaviour
+{
+    bool SoundFlag;
+
+    private CriAtomSource Run;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Run.GetComponent<CriAtomSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SoundFlag)
+        {
+            Run.Play();
+            SoundFlag = false;
+        }
+    }
+
+    void RunFlag()
+    {
+        SoundFlag = true;
+        Debug.Log("yes");
+    }
+
+}
