6e9256284551adf116fc5a894673f5f43629ec88
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-09-01 01:59:54 +0900

    走る音の実装

--- Assets/Script/Serron/RunSound.cs
+++ Assets/Script/Serron/RunSound.cs
@@ -4,29 +4,22 @@ using UnityEngine;
 
 public class RunSound : MonoBehaviour
 {
-    bool SoundFlag;
-
     private CriAtomSource Run;
     // Start is called before the first frame update
     void Start()
     {
-        Run.GetComponent<CriAtomSource>();
+        Run = GetComponent<CriAtomSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SoundFlag)
-        {
-            Run.Play();
-            SoundFlag = false;
-        }
+
     }
 
     void RunFlag()
     {
-        SoundFlag = true;
-        Debug.Log("yes");
+        Run.Play();
     }
 
 }
