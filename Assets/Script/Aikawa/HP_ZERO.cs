05a9dd256908929b5dd15ab99c2380cb5fabc4e0
hisato314 <m0119001bb@edu.teu.ac.jp>
2021-09-15 22:40:11 +0900

    HPゼロの時

---
+++ Assets/Script/Aikawa/HP_ZERO.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP_ZERO : MonoBehaviour
+{
+    /*
+        <<HPがゼロになった時の遷移に使うとき>>
+    
+        1.playerのHPがゼロになったとき
+
+            public HP_ZERO hp_zero;
+
+                と宣言しておいて
+
+            if(!hp_zero.playerHP_ZERO)
+            {
+            }
+
+        2.bossのHPがゼロになったとき
+
+            public HP_ZERO hp_zero;
+
+                と宣言しておいて
+
+            if(!hp_zero.bossHP)
+            {
+            }
+    */
+    
+    //
+    public Slider playerHP;
+    public Slider bossHP;
+
+    public static bool playerHP_ZERO = false;
+    public static bool bossHP_ZERO = false;
+
+    void Update()
+    {
+        if(playerHP.value == 0)
+        {
+            playerHP_ZERO = true;
+        }
+        if(playerHP.value == 0)
+        {
+            bossHP_ZERO = true;
+        }
+    }
+}
