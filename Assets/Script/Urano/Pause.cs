cb6f2fabcc027fd604ea1a7c1b1c0eaffeb5411d
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-09-15 15:32:31 +0900

    ポーズの音入れるの忘れてた

--- Assets/Script/Urano/Pause.cs
+++ Assets/Script/Urano/Pause.cs
@@ -6,9 +6,13 @@ public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
     // Start is called before the first frame update
+
+    public CriAtomSource PauseOpen;
+    public CriAtomSource PauseClose;
     void Start()
     {
-        
+        //PauseOpen = GetComponent<CriAtomSource>();
+        //PauseClose = GetComponent<CriAtomSource>();
     }
 
     // Update is called once per frame
@@ -17,10 +21,12 @@ public class Pause : MonoBehaviour
         if (Time.timeScale == 1.0f && pauseMenu.activeInHierarchy == true)
         {
             pauseMenu.SetActive(false);
+            PauseClose.Play();
         }
         else if(Time.timeScale != 1.0f && pauseMenu.activeInHierarchy == false)
         {
             pauseMenu.SetActive(true);
+            PauseOpen.Play();
         }
     }
 }
