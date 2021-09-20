c355449a3eaf82462859376c8ca3fd73734d82f1
YutaKurosawa <m0119114c4@edu.teu.ac.jp>
2021-09-21 02:25:51 +0900

    餅エフェクトの制御

---
+++ Assets/Script/Kurosawa/MotiTrailEffect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MotiTrailEffect : MonoBehaviour
+{
+    [SerializeField] GameObject TrailEffect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrailEffect.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButton(0))
+        //{
+        //    TrailEffect.SetActive(true);
+        //}
+        //else
+        //{
+        //    TrailEffect.SetActive(false);
+        //}
+    }
+    void OnFire(InputValue input)
+    {
+        var pressed = input.isPressed;
+        TrailEffect.SetActive(true);
+        if (!pressed)
+        {
+            TrailEffect.SetActive(false);
+        }
+    }
+    public void EffectStart()
+    {
+        TrailEffect.SetActive(true);
+    }
+    public void EffectDestroy()
+    {
+        TrailEffect.SetActive(false);
+    }
+}
