d219d47eb86bcaa3a33ba42bd2aafa5dd916383a
hisato314 <m0119001bb@edu.teu.ac.jp>
2021-09-22 00:45:51 +0900

    一時保存

--- Assets/Script/Aikawa/CameraControll.cs
+++ Assets/Script/Aikawa/CameraControll.cs
@@ -6,33 +6,58 @@ using UnityEngine.InputSystem;
 
 public class CameraControll : MonoBehaviour {
     private GameObject mainCameraObj;
-    private bool pause = false;
+    private bool pause = false, pressFlag = false;
     
     private void Start() {
         mainCameraObj = GameObject.FindGameObjectWithTag("MainCamera");
     }
-
+    public void OnFire(){
+        if(!pressFlag) pressFlag = true;
+        else pressFlag = false;
+    }
+    int testCnt = 0;
 	void Update (){
         if(Keyboard.current[Key.Escape].wasPressedThisFrame){
             if(!pause){
-                mainCameraObj.GetComponent<CameraMove>().enabled = false;
-                Time.timeScale = 0.0f;
+                if(pressFlag) pressFlag = false;
                 pause = true;
             }
-            else{
-                mainCameraObj.GetComponent<CameraMove>().enabled = true;
-                Time.timeScale = 1.0f;
-                pause = false;
-            }
+            else pause = false;
         }
         
-        if(Time.timeScale == 0.0f){
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
+	}
+    private void FixedUpdate() {
+        if(!pause && !pressFlag) CursorOff();
+        if(Time.timeScale != 0.0f && !pressFlag) CursorOff();
+
+        if(!pause){
+            mainCameraObj.GetComponent<CameraMove>().enabled = true;
+            if(pressFlag) CursorOn();
+            else{
+                testCnt+=1;
+                Debug.Log(testCnt);
+                CursorOff();
+            }
+            TimeScale_1();
         }
         else{
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
+            mainCameraObj.GetComponent<CameraMove>().enabled = false;
+            CursorOn();
+            TimeScale_0();
         }
-	}
+    }
+    public void CursorOn(){
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void CursorOff(){
+        Cursor.lockState = CursorLockMode.Locked;;
+        Cursor.visible = false;
+    }
+    public void TimeScale_1(){
+        Time.timeScale = 1.0f;
+    }
+    public void TimeScale_0(){
+        Time.timeScale = 0.0f;
+    }
 }
