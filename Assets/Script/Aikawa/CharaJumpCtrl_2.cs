ad3ce1b574a964331c10d8bedb32cbe1c53ac0dd
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-09-24 14:30:07 +0900

    叩きつけ関連のバグの解消

--- Assets/Script/Aikawa/CharaJumpCtrl_2.cs
+++ Assets/Script/Aikawa/CharaJumpCtrl_2.cs
@@ -12,16 +12,23 @@ public class CharaJumpCtrl_2 : MonoBehaviour
     private Animator Moti_spin;
     private string motiStr = "IsMotiSize";
 
+    public bool CanBlend;
+    public bool Tatakituke;
+
     void OnFire(InputValue input)
     {
         bool pressed = input.isPressed;
         if (!pressed)
         {
             this.usagi.SetBool(usagiStr, true);
+            CanBlend = false;
+            Tatakituke = true;
         }
         else
         {
             moti.SetActive(true);
+            CanBlend = true;
+            Tatakituke = false;
             this.usagi.SetBool(usagiStr, false);
             Moti_spin.SetBool(motiStr, false);
         }
@@ -33,6 +40,8 @@ public class CharaJumpCtrl_2 : MonoBehaviour
 
         Moti_spin = moti.GetComponent<Animator>();
         Moti_spin.SetBool(motiStr, false);
+
+        CanBlend = false;
     }
     void Update()
     {
--- Assets/Script/Kurosawa/MotiSize_Anim.cs
+++ Assets/Script/Kurosawa/MotiSize_Anim.cs
@@ -12,17 +12,20 @@ public class MotiSize_Anim : MonoBehaviour
     bool KesuFlag;
     float count = 0.0f;
 
+    bool Tatakituke;
+
+    public GameObject kogane_wait;
+
     void OnFire(InputValue input)
     {
         bool pressed = input.isPressed;
         if (!pressed)
         {
-            this.Moti_main.SetBool(motiStr, true);
-            this.Moti_main.SetBool(motiStr, false);
+            //this.Moti_main.SetBool(motiStr, true);
         }
         else
         {
-            this.Moti_main.SetBool(motiStr, false);
+            //this.Moti_main.SetBool(motiStr, false);
         }
     }
 
@@ -33,20 +36,31 @@ public class MotiSize_Anim : MonoBehaviour
         this.Moti_main.SetBool(motiStr, false);
 
         KesuFlag = false;
+        Tatakituke = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Tatakituke = kogane_wait.GetComponent<CharaJumpCtrl_2>().Tatakituke;
         if (KesuFlag)
         {
             count += Time.deltaTime;
-            if (count > 1.0f)
+            if (count > 0.1f)
             {
                 KesuFlag = false;
                 count = 0.0f;
                 this.gameObject.SetActive(false);
             }
+        }
+
+        if (Tatakituke)
+        {
+            this.Moti_main.SetBool(motiStr, true);
+        }
+        else
+        {
+            this.Moti_main.SetBool(motiStr, false);
         }
     }
     public void ActiveFalse()
