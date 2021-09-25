8652aafd3dec2b6e4c45540453a4763c63c2ba08
YutaKurosawa <m0119114c4@edu.teu.ac.jp>
2021-09-25 11:57:58 +0900

    追加エフェクト実装

--- Assets/Script/Kurosawa/MotiEffect.cs
+++ Assets/Script/Kurosawa/MotiEffect.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class MotiEffect : MonoBehaviour
 {
     [SerializeField] GameObject MotiTrailEffect;
-    [SerializeField] GameObject MotiHurioroshiEffect;
+    [SerializeField] GameObject MotiHurioroshiEffect_01;
+    [SerializeField] GameObject MotiHurioroshiEffect_02;
+    [SerializeField] GameObject MotiHurioroshiEffect_03;
     bool charge = false;
     public float countup = 0.0f;
     public float ActiveTime = 5.0f;
@@ -14,16 +16,26 @@ public class MotiEffect : MonoBehaviour
     void Start()
     {
         MotiTrailEffect.SetActive(false);
-        MotiHurioroshiEffect.SetActive(false);
+        MotiHurioroshiEffect_01.SetActive(false);
+        MotiHurioroshiEffect_02.SetActive(false);
+        MotiHurioroshiEffect_03.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (charge)
+        //{
+        //    countup += Time.deltaTime;
+        //    Debug.Log(countup);
+        //}
+    }
+    private void FixedUpdate()
+    {
         if (charge)
         {
             countup += Time.deltaTime;
-            //Debug.Log(countup);
+            Debug.Log(countup);
         }
     }
 
@@ -40,14 +52,21 @@ public class MotiEffect : MonoBehaviour
     public void EffectDestroy()
     {
         MotiTrailEffect.SetActive(false);
-        MotiHurioroshiEffect.SetActive(false);
+        MotiHurioroshiEffect_01.SetActive(false);
+        MotiHurioroshiEffect_02.SetActive(false);
+        MotiHurioroshiEffect_03.SetActive(false);
         countup = 0.0f;
     }
     public void HurioroshiActive()
     {
+        MotiHurioroshiEffect_03.SetActive(true);
         if (countup >= ActiveTime)
         {
-            MotiHurioroshiEffect.SetActive(true);
+            MotiHurioroshiEffect_01.SetActive(true);
+            if (countup >= ActiveTime * 2)
+            {
+                MotiHurioroshiEffect_02.SetActive(true);
+            }
         }
     }
 }
