1c88050a8d194e5e603509139c39cd7da0e92bcb
YutaKurosawa <m0119114c4@edu.teu.ac.jp>
2021-09-25 15:04:56 +0900

    現状です。今はkogane_waitに色々スクリプトおいてる

--- Assets/Script/Kurosawa/KoganeEffectActive.cs
+++ Assets/Script/Kurosawa/KoganeEffectActive.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class KoganeEffectActive : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject TrailEffect;
+    public GameObject SyogekiEffect_01;
+    public GameObject SyogekiEffect_02;
+    public GameObject SyogekiEffect_03;
+
+    public int EffectCount = 0;
+    public float countup = 0.0f;
+    public float ActiveTime = 5.0f;
+    bool charge = false;
+    public HitControllBurn BurnEnemyHit;
+    public HitControllTemp TempEnemyHit;
     void Start()
     {
-        //TrailEffect.SetActive(false);
+        TrailEffect.SetActive(false);
+        SyogekiEffect_01.SetActive(false);
+        SyogekiEffect_02.SetActive(false);
+        SyogekiEffect_03.SetActive(false);
     }
 
     // Update is called once per frame
@@ -16,8 +30,52 @@ public class KoganeEffectActive : MonoBehaviour
     {
         
     }
+    private void FixedUpdate()
+    {
+        if (charge)
+        {
+            countup += Time.deltaTime;
+            Debug.Log(countup);
+        }
+    }
+
+    void OnFire(InputValue input)
+    {
+        var pressed = input.isPressed;
+        charge = pressed;
+        //TrailEffect.SetActive(pressed);
+    }
     public void RotateActive()
     {
         TrailEffect.SetActive(true);
+    }
+    public void TatakitukeActive()
+    {
+        SyogekiEffect_01.SetActive(true);
+        if (countup >= 5)
+        {
+            SyogekiEffect_02.SetActive(true);
+            Debug.Log("Zikkou");
+            if (countup >= 10)
+            {
+                SyogekiEffect_03.SetActive(true);
+            }
+        }
+    }
+    public void EffectDestroy()
+    {
+        TrailEffect.SetActive(false);
+        SyogekiEffect_01.SetActive(false);
+        SyogekiEffect_02.SetActive(false);
+        SyogekiEffect_03.SetActive(false);
+        countup = 0.0f;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            EffectCount++;
+            Debug.Log("EffectCount");
+        }
     }
 }
--- Assets/Script/Kurosawa/MotiEffect.cs
+++ Assets/Script/Kurosawa/MotiEffect.cs
@@ -9,12 +9,12 @@ public class MotiEffect : MonoBehaviour
     [SerializeField] GameObject MotiHurioroshiEffect_01;
     [SerializeField] GameObject MotiHurioroshiEffect_02;
     [SerializeField] GameObject MotiHurioroshiEffect_03;
-    bool charge = false;
-    public float countup = 0.0f;
-    public float ActiveTime = 5.0f;
-    public int EffectCount = 0;
-    public HitControllBurn BurnEnemyHit;
-    public HitControllTemp TempEnemyHit;
+    //bool charge = false;
+    //public float countup = 0.0f;
+    //public float ActiveTime = 5.0f;
+    //public int EffectCount = 0;
+    //public HitControllBurn BurnEnemyHit;
+    //public HitControllTemp TempEnemyHit;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,66 +33,66 @@ public class MotiEffect : MonoBehaviour
         //    Debug.Log(countup);
         //}
     }
-    private void FixedUpdate()
-    {
-        if (charge)
-        {
-            countup += Time.deltaTime;
-            Debug.Log(countup);
-        }
-    }
+    //private void FixedUpdate()
+    //{
+    //    if (charge)
+    //    {
+    //        countup += Time.deltaTime;
+    //        Debug.Log(countup);
+    //    }
+    //}
 
-    void OnFire(InputValue input)
-    {
-        var pressed = input.isPressed;
-        charge = pressed;
-        MotiTrailEffect.SetActive(pressed);
-    }
-    public void EffectActive()
-    {
-        MotiTrailEffect.SetActive(true);
-    }
-    public void EffectDestroy()
-    {
-        MotiTrailEffect.SetActive(false);
-        MotiHurioroshiEffect_01.SetActive(false);
-        MotiHurioroshiEffect_02.SetActive(false);
-        MotiHurioroshiEffect_03.SetActive(false);
-        countup = 0.0f;
-    }
-    public void HurioroshiActive()
-    {
-        MotiHurioroshiEffect_03.SetActive(true);
-        MotiHurioroshiEffect_02.SetActive(true);
-        MotiHurioroshiEffect_03.SetActive(true);
-        if (EffectCount>=1)
-        {
-            MotiHurioroshiEffect_01.SetActive(true);
-            Debug.Log("Zikkou");
-            if (EffectCount>=5)
-            {
-                MotiHurioroshiEffect_02.SetActive(true);
-            }
-        }
-        //if (countup >= ActiveTime)
-        //{
-        //    MotiHurioroshiEffect_01.SetActive(true);
-        //    if (countup >= ActiveTime * 2)
-        //    {
-        //        MotiHurioroshiEffect_02.SetActive(true);
-        //    }
-        //}
-        //if (EnemyHit.hitCount > 5)
-        //{
+    //void OnFire(InputValue input)
+    //{
+    //    var pressed = input.isPressed;
+    //    charge = pressed;
+    //    MotiTrailEffect.SetActive(pressed);
+    //}
+    //public void EffectActive()
+    //{
+    //    MotiTrailEffect.SetActive(true);
+    //}
+    //public void EffectDestroy()
+    //{
+    //    MotiTrailEffect.SetActive(false);
+    //    MotiHurioroshiEffect_01.SetActive(false);
+    //    MotiHurioroshiEffect_02.SetActive(false);
+    //    MotiHurioroshiEffect_03.SetActive(false);
+    //    countup = 0.0f;
+    //}
+    //public void HurioroshiActive()
+    //{
+    //    MotiHurioroshiEffect_03.SetActive(true);
+    //    MotiHurioroshiEffect_02.SetActive(true);
+    //    MotiHurioroshiEffect_03.SetActive(true);
+    //    if (EffectCount>=1)
+    //    {
+    //        MotiHurioroshiEffect_01.SetActive(true);
+    //        Debug.Log("Zikkou");
+    //        if (EffectCount>=5)
+    //        {
+    //            MotiHurioroshiEffect_02.SetActive(true);
+    //        }
+    //    }
+    //    //if (countup >= ActiveTime)
+    //    //{
+    //    //    MotiHurioroshiEffect_01.SetActive(true);
+    //    //    if (countup >= ActiveTime * 2)
+    //    //    {
+    //    //        MotiHurioroshiEffect_02.SetActive(true);
+    //    //    }
+    //    //}
+    //    //if (EnemyHit.hitCount > 5)
+    //    //{
 
-        //}
-    }
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "enemy")
-        {
-            EffectCount++;
-            Debug.Log("EffectCount");
-        }
-    }
+    //    //}
+    //}
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    if (other.gameObject.tag == "enemy")
+    //    {
+    //        EffectCount++;
+    //        Debug.Log("EffectCount");
+    //    }
+    //}
 }
