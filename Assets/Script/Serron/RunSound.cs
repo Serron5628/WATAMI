c31376ff9fb3365be79e94a3f57fa9ef8951226b
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-09-27 18:39:47 +0900

    no message

--- Assets/Script/Serron/RunSound.cs
+++ Assets/Script/Serron/RunSound.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KoganeSound : MonoBehaviour
+public class RunSound : MonoBehaviour
 {
     public GameObject R;
     private CriAtomSource RAshioto;
