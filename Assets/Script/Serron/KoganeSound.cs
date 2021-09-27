c37e3fa267f5898f4c761ff0563fa985a7229a07
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-09-27 19:02:47 +0900

    Unity上でエラーはいてた

--- Assets/Script/Serron/KoganeSound.cs
+++ Assets/Script/Serron/KoganeSound.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunSound : MonoBehaviour
+public class KoganeSound : MonoBehaviour
 {
     public GameObject R;
     private CriAtomSource RAshioto;
     public GameObject L;
     private CriAtomSource LAshioto;
+    public GameObject DownSound;
+    private CriAtomSource downsound;
     // Start is called before the first frame update
     void Start()
     {
         RAshioto = R.GetComponent<CriAtomSource>();
         LAshioto = L.GetComponent<CriAtomSource>();
+        downsound = DownSound.GetComponent<CriAtomSource>();
     }
 
     // Update is called once per frame
@@ -28,5 +31,9 @@ public class RunSound : MonoBehaviour
     void LRunFlag()
     {
         LAshioto.Play();
+    }
+    void KoganeDownSound()
+    {
+        downsound.Play();
     }
 }
