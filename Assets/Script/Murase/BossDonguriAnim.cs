27b7e29ef35a40cdc1c8b58180912f55d8be498b
AikawaGames <m0119001bb@edu.teu.ac.jp>
2021-09-29 00:06:37 +0900

    地ならし反映

--- Assets/Script/Murase/BossDonguriAnim.cs
+++ Assets/Script/Murase/BossDonguriAnim.cs
@@ -9,32 +9,62 @@ public class BossDonguriAnim : MonoBehaviour
 
     public PlayerHp playerHp;
 
+    public GameObject bossLeftFoot;
+    public GameObject bossRightFoot;
+    public GameObject playerObj;
+
     public bool isLeftFoot = false;
     public bool isRightFoot = false;
 
+    private bool stumpFlag = false;
+
+    void Start(){
+        if(!playerObj)
+            playerObj = GameObject.FindWithTag("Player");
+    }
+
+    void Update() {
+        var playerPos = playerObj.transform.position;
+        var bossLeftFootPos = bossLeftFoot.transform.position;
+        var bossRightFootPos = bossRightFoot.transform.position;
+
+        var distL = Vector3.Distance(
+            new Vector3(playerPos.x, 0, playerPos.z), 
+            new Vector3(bossLeftFootPos.x, 0, bossLeftFootPos.z));
+
+        var distR = Vector3.Distance(
+            new Vector3(playerPos.x, 0, playerPos.z), 
+            new Vector3(bossRightFootPos.x, 0, bossRightFootPos.z));
+        
+        if(distL < 4.7f || distR < 4.7f)
+            stumpFlag = true;
+        else
+            stumpFlag = false;
+    }
+
     // Start is called before the first frame update
-    void RightFoot()
-    {
-        playerHp.Damage_RightStump();
+    void RightFoot(){
+        if(stumpFlag)
+            playerHp.Damage_RightStump();
+
         RstampObj.SetActive(true);
         isRightFoot = true;
     }
 
-    void DRightFoot()
-    {
+    void DRightFoot(){
         RstampObj.SetActive(false);
         isRightFoot = false;
     }
 
-    void LeftFoot()
-    {
-        playerHp.Damage_LeftStump();
+    void LeftFoot(){
+        if(stumpFlag)
+            playerHp.Damage_LeftStump();
+
         LstampObj.SetActive(true);
         isLeftFoot = true;
     }
 
-    void DLeftFoot()
-    {
+    void DLeftFoot(){
         LstampObj.SetActive(false);
         isLeftFoot = false;
     }
