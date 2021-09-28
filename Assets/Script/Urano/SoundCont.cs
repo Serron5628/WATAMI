a37a028cfa5c0f0b3d5b6d0490b4d203e3067d88
Yuta Urano <m0119041e8@edu.teu.ac.jp>
2021-09-28 23:12:34 +0900

    音量調節途中経過

---
+++ Assets/Script/Urano/SoundCont.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundCont : MonoBehaviour
+{
+    public float BGMVol;
+    public float SEVol;
+    public Slider BGMSlider;
+    public Slider SESlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BGMSet()
+    {
+        BGMVol = BGMSlider.value;
+        CriAtom.SetCategoryVolume("Music", BGMVol);
+    }
+
+    public void SESet()
+    {
+        SEVol = SESlider.value;
+        CriAtom.SetCategoryVolume("SFX", SEVol);
+    }
+}
