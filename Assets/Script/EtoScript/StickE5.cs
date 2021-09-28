29337208a842c8e39a73d0192d038ea34e4a640f
m01190442c <m01190442c@edu.teu.ac.jp>
2021-09-28 22:05:03 +0900

    叩きつけの時、くっつかないようにするスクリプト

--- Assets/Script/EtoScript/StickE5.cs
+++ Assets/Script/EtoScript/StickE5.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class StickE5 : MonoBehaviour
 {
     public bool ReParent;
+
+
+    bool SmashOn;
 
     public bool[] m = new bool[7];
 
@@ -12,25 +16,35 @@ public class StickE5 : MonoBehaviour
 
     public DeleteE deleteE;
 
+    MotiBend_Anim MBA;
+
+    public GameObject kogane_wait;
+    CharaJumpCtrl_2 CJC_2;
+    public bool CanBlend;
+
     // Start is called before the first frame update
     void Start()
     {
         ReParent = deleteE.reParent;
+        
         for (int i = 1; i < m.Length; i++)
         {
             m[i] = true;
         }
         m[0] = false;
+
+        CanBlend = kogane_wait.GetComponent<CharaJumpCtrl_2>().CanBlend;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        CanBlend = kogane_wait.GetComponent<CharaJumpCtrl_2>().CanBlend;
     }
 
     void OnTriggerStay(Collider collision)
     {
+        CanBlend = kogane_wait.GetComponent<CharaJumpCtrl_2>().CanBlend;
         for (int i = 0; i < moti.Length; ++i)
         {
             var objName = "mochi";
@@ -38,13 +52,17 @@ public class StickE5 : MonoBehaviour
 
             if (collision.gameObject.name == objName && m[i])
             {
-                this.gameObject.transform.parent = moti[i].gameObject.transform;
-
-                // このインデックス以外のmのboolをfalseに設定
-                for (int j = 0; j < moti.Length; ++j)
+                if(CanBlend == true)
                 {
-                    if (j != i) m[j] = false;
+                    this.gameObject.transform.parent = moti[i].gameObject.transform;
+
+                    // このインデックス以外のmのboolをfalseに設定
+                    for (int j = 0; j < moti.Length; ++j)
+                    {
+                        if (j != i) m[j] = false;
+                    }
                 }
+                
             }
         }
     }
--- Assets/Script/Serron/HitControllBurn.cs
+++ Assets/Script/Serron/HitControllBurn.cs
@@ -17,6 +17,10 @@ public class HitControllBurn : MonoBehaviour
 
     MotiRotate rotate;
 
+    public GameObject kogane_wait;
+    CharaJumpCtrl_2 CJC_2;
+    bool CanBlend;
+
     private CriAtomSource Enemy;
     // Start is called before the first frame update
 
@@ -27,6 +31,7 @@ public class HitControllBurn : MonoBehaviour
         stickFlag = false;
         Enemy = GetComponent<CriAtomSource>();
         rotate = Kogane.GetComponent<MotiRotate>();
+        CanBlend = kogane_wait.GetComponent<CharaJumpCtrl_2>().CanBlend;
     }
 
     void FixedUpdate()
@@ -64,11 +69,15 @@ public class HitControllBurn : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        CanBlend = kogane_wait.GetComponent<CharaJumpCtrl_2>().CanBlend;
         //ヒットした回数を数えてる。
         if (other.gameObject.tag == "Moti")
         {
-            hitCount++;
-            //Debug.Log("hit!");
+            if (CanBlend)
+            {
+                hitCount++;
+                //Debug.Log("hit!");
+            }
         }
 
     }
